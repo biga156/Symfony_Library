d7825bcf2a1c4612283a986b1c4b1994e46473ec
biga156 <biga156@gmail.com>
2020-11-23 15:05:35 +0100

    before relations and db

---
+++ src/Entity/Loan.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LoanRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LoanRepository::class)
+ */
+class Loan
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $udatedAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $searchable;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUdatedAt(): ?\DateTimeInterface
+    {
+        return $this->udatedAt;
+    }
+
+    public function setUdatedAt(?\DateTimeInterface $udatedAt): self
+    {
+        $this->udatedAt = $udatedAt;
+
+        return $this;
+    }
+
+    public function getSearchable(): ?bool
+    {
+        return $this->searchable;
+    }
+
+    public function setSearchable(bool $searchable): self
+    {
+        $this->searchable = $searchable;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Ressource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RessourceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RessourceRepository::class)
+ */
+class Ressource
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $cote;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $availability;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $picture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCote(): ?string
+    {
+        return $this->cote;
+    }
+
+    public function setCote(string $cote): self
+    {
+        $this->cote = $cote;
+
+        return $this;
+    }
+
+    public function getAvailability(): ?bool
+    {
+        return $this->availability;
+    }
+
+    public function setAvailability(bool $availability): self
+    {
+        $this->availability = $availability;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
